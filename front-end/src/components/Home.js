451cd19d08d0ee7e267ac5a88354dad832ba7826
wajdi1996 <wajdibelhajsimplonenda@gmail.com>
2021-09-04 20:11:38 +0200

    first commit

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -457,53 +457,6 @@ export default function Home(){
     </section>
 
    
-    <footer class="footer bg-dark">
-        <div class="container">
-            <div class="row">
-               
-                <div class="col-lg-6 text-center text-lg-left my-auto  wow zoomIn">
-                    <ul class="list-inline mb-2">
-                        <li class="list-inline-item">
-                            <a href="#">About</a>
-                        </li>
-                        <li class="list-inline-item">⋅</li>
-                        <li class="list-inline-item">
-                            <a href="#">Contact</a>
-                        </li>
-                        <li class="list-inline-item">⋅</li>
-                        <li class="list-inline-item">
-                            <a href="#">Terms of Use</a>
-                        </li>
-                        <li class="list-inline-item">⋅</li>
-                        <li class="list-inline-item">
-                            <a href="#">Privacy Policy</a>
-                        </li>
-                    </ul>
-                    <p class="text-muted small mb-4 mb-lg-0">© Mojoave 2018. All Rights Reserved.</p>
-                </div>
-                <div class="col-lg-6 text-center text-lg-right my-auto  wow zoomIn">
-                    <ul class="list-inline mb-0">
-                        <li class="list-inline-item mr-3">
-                            <a href="#">
-                                <i class="fa fa-facebook fa-2x fa-fw"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item mr-3">
-                            <a href="#">
-                                <i class="fa fa-twitter fa-2x fa-fw"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#">
-                                <i class="fa fa-instagram fa-2x fa-fw"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-    
         
         <Footer />
         </>
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import {createStore} from 'redux'
+// import {Provider} from 'react-redux'
+// import reducer from './reducers/rootReducer'
 
+//  const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <Provider store={store}>
+    <React.StrictMode>
     <App />
   </React.StrictMode>,
+ 
   document.getElementById('root')
 );
 
+/* </Provider>, */
